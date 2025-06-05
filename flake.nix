738bebd7a620f2f988c3837ede77a0edc7812ae2
toms55 <tom.storey25@gmail.com>
2025-06-05 23:14:38 +1000

    shared alacritty config

--- flake.nix
+++ flake.nix
@@ -5,29 +5,53 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.05";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, darwin }:
-    {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./nixos/configuration.nix
-            ./nixos/hardware-configuration.nix
-          ];
-        };
-      };
-
-      darwinConfigurations = {
-        Toms-MacBook-Pro = darwin.lib.darwinSystem {
-          system = "aarch64-darwin"; 
-          modules = [
-            ./darwin/configuration.nix
-          ];
-        };
+  outputs = { self, nixpkgs, darwin, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+          ./nixos/hardware-configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useUserPackages = true;
+            home-manager.users.tom = { pkgs, ... }: {
+              home.username = "tom";
+              home.homeDirectory = "/home/tom";
+              home.stateVersion = "23.11";
+              programs.alacritty.enable = true;
+              xdg.configFile."alacritty/alacritty.toml".source = ./config/alacritty.toml;
+            };
+          }
+        ];
       };
     };
+
+    darwinConfigurations = {
+      Toms-MacBook-Pro = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./darwin/configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useUserPackages = true;
+            home-manager.users.tom = { pkgs, ... }: {
+              home.username = "tom";
+              home.homeDirectory = "/Users/tom";
+              home.stateVersion = "23.11";
+              programs.alacritty.enable = true;
+              xdg.configFile."alacritty/alacritty.toml".source = ./config/alacritty.toml;
+            };
+          }
+        ];
+      };
+    };
+  };
 }
+
 
 
