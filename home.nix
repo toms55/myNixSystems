0565284c28b52cd9c20ebeea08faa7ef23153eed
Tom <tom@Toms-MacBook-Pro.local>
2025-06-05 23:28:47 +1000

    fixed home.nix

--- home.nix
+++ home.nix
@@ -1,15 +1,12 @@
-{ system, username }: { pkgs, ... }: {
-
+{ system, username }: {
   home-manager.useUserPackages = true;
-  home-manager.users.${username} = {
-    inherit username;
+  home-manager.users.${username} = { pkgs, ... }: {
+    home.username = username;
     home.homeDirectory = if system == "darwin" then "/Users/${username}" else "/home/${username}";
     home.stateVersion = "23.11";
 
     programs.alacritty.enable = true;
-
     xdg.configFile."alacritty/alacritty.toml".source = ./config/alacritty.toml;
-  
   };
 }
 
