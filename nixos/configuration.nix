dc248a6129073858b3bf728b4efca071405ed844
toms55 <tom.storey25@gmail.com>
2025-08-05 13:55:41 +1000

    broken, but testing

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,23 +88,15 @@
     dates = "weekly";
     persistent = true;
   };
-  
-  # services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware.nvidia = {
     open = false;
     modesetting.enable = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     powerManagement.enable = true;
-    nvidiaSettings = true;
-    forceFullCompositionPipeline = true;
   };
 
-  boot.kernelParams = [
-    "nvidia_drm.modeset=1"
-    "nvidia_drm.fbdev=1"
-    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-    "nvidia.NVreg_UsePageAttributeTable=1"
-  ];
+  boot.kernelParams = [ "nvidia_drm.modeset=1" ];
   
   # vm stuff
   virtualisation.libvirtd.enable = true;
@@ -212,8 +204,7 @@
 
     # qtile
     python3Packages.qtile
-    wlr-randr
-    
+
     wofi
     nh
 
@@ -304,8 +295,6 @@
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_CACHE_HOME = "$HOME/.cache";
     XDG_DATA_HOME = "$HOME/.local/share";
-
-    XWAYLAND_VIDEOSYNC = "1";
 
     XDG_SESSION_TYPE = "wayland";
     ELECTRON_OZONE_PLATFORM_HINT = "wayland";
