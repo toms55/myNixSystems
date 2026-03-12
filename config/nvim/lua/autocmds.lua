23aaf2306f0a0b9948b95121b14958b2b201669a
Tom <tom@MacBookPro.gateway>
2026-03-12 18:19:59 +1100

    added autosaving nvim

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -1,1 +1,14 @@
 require "nvchad.autocmds"
+local autosave = vim.api.nvim_create_augroup("Autosave", { clear = true })
+
+vim.api.nvim_create_autocmd(
+  { "InsertLeave", "TextChanged", "FocusLost" },
+  {
+    group = autosave,
+    callback = function()
+      if vim.bo.modified and vim.bo.buftype == "" then
+        vim.cmd("silent write")
+      end
+    end,
+  }
+)
